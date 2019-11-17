a347b086638cbca1caba57ccde5021b258a9ae5d
Fatemeh Pakpour <fatemehpakpour64@gmail.com>
2019-11-17 23:45:01 +0100

    added the multimath project

---
+++ app/app.js
@@ -0,0 +1,5 @@
+function startGame() {
+    var messageElement = document.getElementById("message");
+    messageElement.innerText = "welcom";
+}
+document.getElementById("start-game").addEventListener("click", startGame);
